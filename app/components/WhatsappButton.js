76739db8fa2527dfe7f6ccdbf3b2f04a5a222a99
Divyansh Rana <99189937+styloon31@users.noreply.github.com>
2025-03-13 00:49:07 +0530

    wa

--- app/components/WhatsappButton.js
+++ app/components/WhatsappButton.js
@@ -3,7 +3,7 @@ import { BsWhatsapp } from "react-icons/bs"; // Import WhatsApp icon from react-
 export default function WhatsappButton() {
   return (
     <a
-      href="https://wa.me/8882382276" // Replace with the WhatsApp number (country code + number)
+      href="https://wa.me/918882382276" // Replace with the WhatsApp number (country code + number)
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 right-4 md:bottom-6 md:right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition z-50"
